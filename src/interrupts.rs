3ba2ea0e8f8b3ebd01c2b589677dd6502361775d
QMEOWQ <3188986027@qq.com>
2024-11-06 17:00:34 +0800

    first commit os_kernel_double_fault_part on 2024/11/06

---
+++ src/interrupts.rs
@@ -0,0 +1,49 @@
+use crate::gdt;
+use crate::println;
+use lazy_static::lazy_static;
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame}; // 可以让变量在第一次取值时进行初始化而不是在编译时，避免数据竞争
+
+// 理论上 static mut 类型的变量很容易形成数据竞争，所以需要用 unsafe 代码块 修饰调用语句
+//static mut IDT: InterruptDescriptorTable = InterruptDescriptorTable::new();
+
+lazy_static! {
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        //idt.double_fault.set_handler_fn(double_fault_handler);
+        //after add gdt
+        unsafe {
+            idt.double_fault.set_handler_fn(double_fault_handler)
+            .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
+        }
+
+        idt
+    };
+}
+
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: InterruptStackFrame,
+    _error_code: u64,
+) -> ! {
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame)
+}
+
+pub fn init_idt() {
+    IDT.load();
+
+    // unsafe {
+    //     //let mut idt = InterruptDescriptorTable::new();
+    //     IDT.breakpoint.set_handler_fn(breakpoint_handler);
+    //     IDT.load();
+    // }
+}
+
+extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
+    println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+#[test_case]
+fn test_breakpoint_exception() {
+    // 触发断点异常
+    x86_64::instructions::interrupts::int3();
+}
