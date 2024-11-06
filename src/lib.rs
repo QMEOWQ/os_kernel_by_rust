52914dfd5f9ef2564e97c5f077be996c0cca7d77
QMEOWQ <3188986027@qq.com>
2024-11-06 17:49:02 +0800

    first commit heap_alloation_part_added on 2024/11/06

---
+++ src/lib.rs
@@ -0,0 +1,117 @@
+#![no_std]
+#![cfg_attr(test, no_main)]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+#![feature(abi_x86_interrupt)] // 用于 interrupt.rs
+
+pub mod gdt;
+pub mod interrupts;
+pub mod memory;
+pub mod serial;
+pub mod vga_buffer;
+pub mod allocator;
+
+extern crate alloc;
+
+use core::panic::PanicInfo;
+use x86_64::instructions::hlt;
+
+pub fn hlt_loop() -> ! {
+    loop {
+        // 让CPU在下一个中断触发之前休息一下
+        x86_64::instructions::hlt();
+    }
+}
+
+// 封装一个加载gdt和idt的函数
+pub fn init() {
+    gdt::init();
+    interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize() };
+    // x86_64 crate 中的 interrupts::enable 会执行特殊的 sti ("set interrupts") 指令来启用外部中断
+    // 当试着执行 cargo run 后，double fault 异常几乎是立刻就被抛出了
+    x86_64::instructions::interrupts::enable();
+}
+
+// 实现自动添加打印语句
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+pub fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+pub fn test_panic_handler(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    hlt_loop();
+    //loop {}
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+/*************
+**** test ****
+*************/
+
+// 可以执行cargo test 运行所有测试
+// 或执行cargo test --lib 来运行 lib.rs 及其子模块中包含的测试
+
+// Entry point for `cargo test`
+#[cfg(test)]
+use bootloader::{entry_point, BootInfo};
+
+#[cfg(test)]
+entry_point!(test_kernel_main);
+
+#[cfg(test)]
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
+    init();
+    test_main();
+    hlt_loop();
+}
+
+// pub extern "C" fn _start() -> ! {
+//     init();
+//     test_main();
+//     hlt_loop();
+//     //loop {}
+// }
+
+// #[cfg(test)]
+// #[panic_handler]
+// fn panic(info: &PanicInfo) -> ! {
+//     test_panic_handler(info)
+// }
---
+++ tests/heap_allocation.rs
@@ -0,0 +1,72 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(os_by_rust::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+extern crate alloc;
+
+use alloc::boxed::Box;
+use alloc::vec::Vec;
+use bootloader::{entry_point, BootInfo};
+use core::panic::PanicInfo;
+use os_by_rust::{allocator::HEAP_SIZE, memory::BootInfoFrameAllocator};
+use x86_64::PhysAddr;
+
+entry_point!(main);
+
+fn main(boot_info: &'static BootInfo) -> ! {
+    use os_by_rust::allocator;
+    use os_by_rust::memory::{self, BootInfoFrameAllocator};
+    use x86_64::VirtAddr;
+
+    os_by_rust::init();
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { os_by_rust::memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed!");
+
+    test_main();
+    loop {}
+    //unimplemented!();
+}
+
+#[test_case]
+// 测试是否未发生分配错误
+fn simple_allocation() {
+    let heap_val_1 = Box::new(42);
+    let heap_val_2 = Box::new(1337);
+    assert_eq!(*heap_val_1, 42);
+    assert_eq!(*heap_val_2, 1337);
+}
+
+// 测试大型分配
+#[test_case]
+fn large_vec() {
+    let n = 1001;
+    let mut vec = Vec::new();
+    for i in 0..n {
+        vec.push(i);
+    }
+    assert_eq!(vec.len(), n);
+    //assert_eq!(vec.iter().sum::<u64>(), n * (n - 1) / 2);
+    assert_eq!(
+        vec.iter().map(|&x| x as u64).sum::<u64>(),
+        (n * (n - 1) / 2).try_into().unwrap()
+    );
+}
+
+// 测试多重分配
+// 此测试可确保分配器将释放的内存重新用于后续分配，否则它将耗尽内存
+#[test_case]
+fn many_boxes() {
+    for i in 0..HEAP_SIZE {
+        let x = Box::new(i);
+        assert_eq!(*x, i);
+    }
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    os_by_rust::test_panic_handler(info);
+}
